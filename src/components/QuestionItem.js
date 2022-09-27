37cbd9d09659a512114ad0a07b466d3481703462
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-09-27 16:22:57 +0300

    assignment

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteClick}) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -17,7 +17,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={onDeleteClick}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+    function fetchingQuiz(){
+      fetch('http://localhost:4000/questions')
+      .then(response => response.json())
+      .then((data) => { setQuestions(data)})
+  }
+
+    useEffect(() => {
+      return ( fetchingQuiz())
+    }, [])
+
+
+    function deleteQuestion(id){
+      fetch(`http://localhost:4000/questions${id}`, {
+        method: 'DELETE',
+      })
+      .then(response => response.json())
+      .then(() => {
+        const updatedtedQuizList = questions.filter(question => question.id !== id)
+        setQuestions(updatedtedQuizList)
+      })
+    }
+
+
+    const renderQuestion = questions.map((question) => <QuestionItem key={question.id} 
+    question={question} 
+    onDeleteClick={deleteQuestion}/>)
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{renderQuestion}</ul>
     </section>
   );
 }
